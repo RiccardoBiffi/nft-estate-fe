6bef6687ed584363b993010100f917a8dc21f7b6
Riccardo Biffi <biffi.riccardo91@gmail.com>
2023-11-20 20:10:16 +0100

    Fix alchemy config

--- src/contexts/AlchemySettings.ts
+++ src/contexts/AlchemySettings.ts
@@ -3,7 +3,7 @@ import { Network } from 'alchemy-sdk';
 
 // security The api key is in cleartext, never do this in production
 const settings = {
-    apiKey: process.env.ALCHEMY_ARBITRUM_SEPOLIA_API_KEY,
+    apiKey: process.env.REACT_APP_ALCHEMY_ARBITRUM_SEPOLIA_API_KEY,
     network: Network.ARB_SEPOLIA,
   }
 
