745127ee95b6c5088ef65572b3c83557ecac7f6e
Riccardo Biffi <biffi.riccardo91@gmail.com>
2023-11-20 18:34:26 +0100

    Update setting to refer arbitrum sepolia

--- src/contexts/AlchemySettings.ts
+++ src/contexts/AlchemySettings.ts
@@ -3,8 +3,8 @@ import { Network } from 'alchemy-sdk';
 
 // security The api key is in cleartext, never do this in production
 const settings = {
-    apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
-    network: Network.ETH_MAINNET,
+    apiKey: process.env.ALCHEMY_ARBITRUM_SEPOLIA_API_KEY,
+    network: Network.ARB_SEPOLIA,
   }
 
 export const AlchemySDKSettings = createContext(settings);
